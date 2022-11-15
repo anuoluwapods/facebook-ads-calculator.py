accdfbd3050feb9139120083a93e89d0c9c29277
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-16 00:26:38 +0100

    Update Cost_Per_Action.py

--- ~/app/pages/Cost_Per_Action.py
+++ ~/app/pages/Cost_Per_Action.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Calculating the cost per action of an ad')
+st.write('Calculating the cost per action of an ad')
 
 cost = st.number_input('Enter the cost per click')
 click = st.number_input('Enter the average clicks per sale')
 
 if st.button('💰'):
   cpa = click * cost
-  st.write("Cost Per Action Is:" cpa)
+  st.write("Cost Per Action Is:". format(cpa))
   
  
   
