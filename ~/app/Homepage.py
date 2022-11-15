3893d7defc235beea87b6b6720e5e2c92ba44428
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-15 23:49:47 +0100

    Create Homepage.py

---
+++ ~/app/Homepage.py
@@ -0,0 +1,19 @@
+#from PIL import Image
+import streamlit as st
+
+# setting image
+# image = Image.open('image.png')
+
+
+col1, col2 = st.columns(2)
+
+col1.header("Facebook Ad Calculator")
+title_1 =  '<p style="font-family:sans-serif; color:White;">To Calculate Facebook ads</p>'
+#col2.image(image)
+
+
+col1.write("[![Project Page](https://img.icons8.com/ios-glyphs/20/ffffff/github.png)](https://github.com/anuoluwapods/facebook-ads-calculator.py) Github") 
+col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods) Instagram")
+col1.write("[![Instagram page](https://img.icons8.com/color/20/null/twitter--v1.png)](https://twitter.com/AnuoluwapoDs) Twitter")
+col1.write("[![Instagram page](https://img.icons8.com/color/20/null/linkedin.png)](https://www.linkedin.com/in/anuoluwapo-abiodun-balogun-64b977186/) LinkedIn")
+col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/facebook-new.png)](https://facebook.com/ifeoluwapo.balogun) Facebook")
