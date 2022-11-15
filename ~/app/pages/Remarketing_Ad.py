8d866e61560ce6e2229ba5b644e1c7b95b5cd059
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-16 00:37:01 +0100

    Create Remarketing_Ad.py

---
+++ ~/app/pages/Remarketing_Ad.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Remarketing Ad')
+
+cpa = st.number_input('Enter the CPA Value')
+rad = st.number_input('Enter Cost of remarketing ad')
+curr = st.text_input('Enter currency symbol')
+
+if st.button('💰'):
+  rmad = cpa * rad
+  st.write("Cost Per Action = {}{}". format(curr, rmd))
