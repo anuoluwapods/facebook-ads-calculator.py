2a7ecb61e52f48e3ba1a05a948d31449c4bf500f
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-16 00:32:42 +0100

    Update Cost_Per_Action.py

--- ~/app/pages/Cost_Per_Action.py
+++ ~/app/pages/Cost_Per_Action.py
@@ -6,10 +6,11 @@ st.title('Cost Per Action Of An Ad')
 
 cost = st.number_input('Enter the cost per click')
 click = st.number_input('Enter the average clicks per sale')
+curr = st.text_input('Enter currency symbol')
 
 if st.button('💰'):
   cpa = click * cost
-  st.write("Cost Per Action = {} Naira". format(cpa))
+  st.write("Cost Per Action = {} {}". format(cpa, curr))
   
  
   
