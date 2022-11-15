9d85747abacf54095b955c9b3eaa6d13438ea6c5
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-15 23:57:41 +0100

    Create Cost_Per_Action.py

---
+++ ~/app/pages/Cost_Per_Action.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Calculating the cost per action of an ad')
+
+cost = st.text_input('Enter the cost per click')
+clicks = st.text_input('Enter the average clicks per sale')
+
+if st.button('💰'):
+  costs * clicks
