b142c3af83a00ceb5ae6f0ade46aadadbf9a5eef
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-16 01:17:24 +0100

    Update Ads_Frequency.py

--- ~/app/pages/Ads_Frequency.py
+++ ~/app/pages/Ads_Frequency.py
@@ -7,8 +7,7 @@ st.write('A good ad frequency is around 1 to 2')
 
 impr = st.number_input('Enter the impression value')
 reac = st.number_input('Enter the reach value')
-curr4 = st.text_input('Enter currency symbol')
 
 if st.button('💰'):
   adfreq = (imp / reac)
-  st.write("Conversion Rate ROI = {}{}". format(curr4, adfreq))
+  st.write("Conversion Rate ROI = {}{". format(adfreq))
