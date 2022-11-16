4661f904c2dbfd21bfedae77e4505b10e19dbfb1
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-16 01:09:21 +0100

    Create CVR_ROI.py

---
+++ ~/app/pages/CVR_ROI.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Conversion Rate ROI')
+
+cvrv = st.number_input('Enter the conversion rate value')
+crvc = st.number_input('Enter the cconversion per clicks')
+curr3 = st.text_input('Enter currency symbol')
+
+if st.button('💰'):
+  cvrvp = (cvrv / 100)
+  cpvroi = (crvc * cvrvp)
+  st.write("Conversion Rate ROI = {}{}". format(curr3, cvroi))
